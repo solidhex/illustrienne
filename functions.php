b31279c09fb5eb81092e7d1a2b261874fa70638b
Patrick Jackson <pj@solidhex.com>
2014-10-14 12:58:55 -0700

    search, featured posts into sidebar.

--- functions.php
+++ functions.php
@@ -23,6 +23,14 @@ add_theme_support( 'post-thumbnails' );
  * Get Attached Images Plugin
  */
 
+/**
+ * Custom Image Sizes
+ * http://codex.wordpress.org/Function_Reference/add_image_size
+ */
+
+// add_image_size( 'category-thumb', 300 ); // 300 pixels wide (and unlimited height)
+// add_image_size( 'homepage-thumb', 220, 180, true ); // (cropped)
+
 function get_attached_images( $args=null )
 {
 	
@@ -96,8 +104,44 @@ function illustrienne_widgets()
 		'before_title' => '<h2>',
 		'after_title' => '</h2>'
 	) );
+	
+	register_sidebar( array(
+		'name' => 'Ads',
+		'id' => 'ads',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '',
+		'after_title' => ''
+	) );
 }
 
 add_action( 'widgets_init', 'illustrienne_widgets' );
+
+function updateNumbers() {
+    /* numbering the published posts, starting with 1 for oldest;
+    / creates and updates custom field 'incr_number';
+    / to show in post (within the loop) use <?php echo get_post_meta($post->ID,'incr_number',true); ?>
+    / alchymyth 2010 
+	/ http://wordpress.stackexchange.com/questions/13083/display-post-number-not-post-id-number
+	*/
+    global $wpdb;
+    $querystr = "SELECT $wpdb->posts.* FROM $wpdb->posts 
+                 WHERE $wpdb->posts.post_status = 'publish' 
+                 AND $wpdb->posts.post_type = 'post' 
+                 ORDER BY $wpdb->posts.post_date ASC";
+    $pageposts = $wpdb->get_results($querystr, OBJECT);
+    $counts = 0 ;
+    if ($pageposts):
+    foreach ($pageposts as $post):
+        $counts++;
+        add_post_meta($post->ID, 'incr_number', $counts, true);
+        update_post_meta($post->ID, 'incr_number', $counts);
+    endforeach;
+endif;
+}  
+
+add_action ( 'publish_post', 'updateNumbers', 11 );
+add_action ( 'deleted_post', 'updateNumbers' );
+add_action ( 'edit_post', 'updateNumbers' );
 
 ?>
--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 				<?php if ( has_post_thumbnail() ) {
 					the_post_thumbnail();
 				} ?>
-				
+				<?php echo $count; ?>
+				<?php echo get_post_meta( $post->ID, 'incr_number', true ); ?>
 				<header class="entry-header">
 					<?php if ( is_single() ) : ?>
 					<h1 class="entry-title"><?php the_title(); ?></h1>
@@ -24,7 +25,11 @@
 				</div>
 				
 			</article>
-    <?php endwhile; else : ?>
+
+    <?php endwhile; ?>
+		<?php posts_nav_link('','','&laquo; Previous Entries') ?>
+		<?php posts_nav_link('','Next Entries &raquo;','') ?>
+	<?php else : ?>
 
     	<!-- The very first "if" tested to see if there were any Posts to -->
     	<!-- display.  This "else" part tells what do if there weren't any. -->
@@ -39,10 +44,65 @@
 	<aside role="complementary">
 		
 		<?php get_template_part( 'partials/social' ); ?>
-		
+				
 		<?php if ( is_active_sidebar ('sidebar') ): ?>
 			<?php dynamic_sidebar( 'sidebar' ); ?>
 		<?php endif ?>
+		
+		<form action="<?php echo home_url( '/' ); ?>" method="get" class="search-form">
+			<input type="search" name="s" value="<?php echo get_search_query() ?>" />
+		</form>
+		
+		<?php if ( is_active_sidebar ('ads') ): ?>
+			<?php dynamic_sidebar( 'ads' ); ?>
+		<?php endif ?>
+				
+		<section class="favorites">
+			<h2>gallery of favorites</h2>
+		
+			<div>
+				
+			<?php
+			
+				$args = array(
+					'post_type' => 'post',
+					'meta_query' => array(
+						array(
+							'key' => 'featured_post',
+							'value' => '1'
+						)
+					)
+				 );
+				 
+				 $posts = get_posts( $args );
+				 
+				 if ( $posts ) :
+			?>
+			
+			<ul>
+			
+			<?php foreach ( $posts as $post ): ?>
+				<li>
+					<figure>
+						<?php echo get_the_post_thumbnail( $post->ID, 'thumbnail' ); ?>
+						<figcaption>
+							<section>
+								<div class="isbn">
+									<span class="no">No</span> <span class="separator">/</span> <span class="issue"><?php echo get_post_meta( $post->ID, 'incr_number', true ); ?></span>
+								</div>
+							</section>
+						</figcaption>
+					</figure>
+					<h3><?php echo wp_trim_words( $post->post_title, 7, '' ); ?></h3>
+				</li>
+			<?php endforeach ?>
+			
+			</ul>
+			
+			<?php endif; ?>
+				
+			</div>
+		</section>
 		
 	</aside>
 </div><!-- eof #container -->
