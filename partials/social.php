f04d06b2a15dbea775feae55385f8100f4af8355
Patrick Jackson <pj@solidhex.com>
2015-01-25 17:46:22 -0800

    add in tumblr

--- partials/social.php
+++ partials/social.php
@@ -3,4 +3,5 @@
 	<a href="https://twitter.com/theillustrienne" class="twitter" target="_blank"></a>
 	<a href="http://www.pinterest.com/theillustrienne/" class="pinterest" target="_blank"></a>
 	<a href="http://instagram.com/the_illustrienne" class="instagram" target="_blank"></a>
+	<a href="http://thedailyillustrienne.tumblr.com/" class="tumblr" target="_blank"></a>
 </div>
